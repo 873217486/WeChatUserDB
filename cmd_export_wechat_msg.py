2e782acaba830be6317b027c1e36231db25408ec
Sam <873217486@qq.com>
2023-02-24 04:45:25 +0800

    bug fix: 聊天记录db文件有多个，会导致导出记录不完整

--- cmd_export_wechat_msg.py
+++ cmd_export_wechat_msg.py
@@ -4,6 +4,7 @@ import os
 import re
 import shutil
 import sqlite3
+import time
 
 import connect_sqlite_tools
 import get_info
@@ -13,13 +14,17 @@ from decode_img import get_code
 print("\n确保微信已登录，否则聊天记录导出失败")
 print("正在获取微信秘钥...")
 base64pwd_wxid = get_info.get_key(0)
+# base64pwd_wxid = ('05eIFhsEQ6yd4e9cJ1CxklXFtbuh80eGvEDabb8O6UM=', 'wxid_f9ncqun8hz2i22')
 key = base64.standard_b64decode(base64pwd_wxid[0])
+# 当前：C:\Users\Administrator\Documents\WeChat Files\
 wechatFilePath = 0
+# 当前：jiangshan873217486
+wechatId = base64pwd_wxid[1]
 print("\n获取秘钥成功", base64pwd_wxid, "\n")
 
 
 def decrypt_db(dbFile):
-    msgDB = wechatFilePath + base64pwd_wxid[1] + dbFile
+    msgDB = wechatFilePath + wechatId + dbFile
     connect_sqlite_tools.decrypt_sqlite_file(msgDB, key)
 
 
@@ -32,6 +37,15 @@ def ex_sql(db, sql, params=()):
         result.append(row)
     conn.close()
     return result
+
+
+# 微信聊天记录很多的时候会生成多个MSG0 MSG1 MSG2.db
+# 检查已经产生了多少个MSG.db
+def check_count_msg_db():
+    count_msg_db = 0
+    while "MSG" + str(count_msg_db) + ".db" in os.listdir(wechatFilePath + wechatId + '\\Msg\\Multi\\'):
+        count_msg_db += 1
+    return count_msg_db
 
 
 def decode_dat(file_path):
@@ -63,48 +77,110 @@ if __name__ == '__main__':
     if not wechatFilePath:
         wechatFilePath = "C:\\Users\\Administrator\\Documents\\WeChat Files\\"
 
+    count_msg_db = check_count_msg_db()
     # 2 复制微信数据库
-    decrypt_db("\\Msg\\Multi\\MSG0.db")
+    for i in range(count_msg_db):
+        decrypt_db("\\Msg\\Multi\\MSG" + str(i) + ".db")
     decrypt_db("\\Msg\\FTSContact.db")
 
-    # 3 查询数据库 选择需要导出的聊天记录
-    sql = '''   SELECT
-                    g.groupTalkerId AS groupTalkerId,
-                    a.c1nickname 
+    # 3 输入需要导出的群聊名称 默认：小台风售后退款群
+    # 22431475380@chatroom
+    export_group_id = ''
+    while not export_group_id:
+        export_group_name = input('输入需要导出的群聊名或微信名（！不是微信备注） 不输入时使用默认导出：小台风售后退款群')
+        if not export_group_name:
+            export_group_name = '小台风售后退款群'
+        # 根据群名查询群ID：小台风售后退款群 -> 22431475380@chatroom
+        sql = '''
+                SELECT
+                    userName 
                 FROM
-                    ( SELECT groupTalkerId FROM FTSChatroom15_MetaData GROUP BY groupTalkerId ) g
-                    LEFT JOIN FTSContact15_content a ON a.docid = g.groupTalkerId 
-                WHERE
-                    a.c1nickname <> ''
-                    '''
-    groupArray = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_FTSContact.db', sql)
-    i = 1
-    print('------------------------')
-    for groupRow in groupArray:
-        print(str(i) + "：" + groupRow[1])
-        i = i + 1
-    print('------------------------')
+                    NameToId 
+                    LIMIT ( SELECT entityId FROM FTSContact15_MetaData WHERE docid = ( SELECT docid FROM FTSContact15_content WHERE c1nickname = ? ) ) - 1,1
+        '''
+        try:
+            export_group_id = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_FTSContact.db', sql, [export_group_name])[0][
+                0]
+        except:
+            print("没有找到该群聊或微信名\n\n")
 
-    inputIndex = input("请输入数字选择群聊：\n")
-    # 327
-    groupTalkerId = groupArray[int(inputIndex) - 1][0]
-    sql = 'SELECT * from NameToId limit ' + str(groupTalkerId - 1) + ',1'
-    # 18972836328@chatroom
-    groupTalkerId = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_FTSContact.db', sql)[0][0]
+    # 3 查询数据库 选择需要导出的聊天记录
+    # sql = '''   SELECT
+    #                 g.groupTalkerId AS groupTalkerId,
+    #                 a.c1nickname
+    #             FROM
+    #                 ( SELECT groupTalkerId FROM FTSChatroom15_MetaData GROUP BY groupTalkerId ) g
+    #                 LEFT JOIN FTSContact15_content a ON a.docid = g.groupTalkerId
+    #             WHERE
+    #                 a.c1nickname <> ''
+    #                 '''
+    # groupArray = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_FTSContact.db', sql)
+    # i = 1
+    # print('------------------------')
+    # for groupRow in groupArray:
+    #     print(str(i) + "：" + groupRow[1])
+    #     i = i + 1
+    # print('------------------------')
+    #
+    # inputIndex = input("请输入数字选择群聊：\n")
+    # # 327
+    # groupTalkerId = groupArray[int(inputIndex) - 1][0]
+    # sql = 'SELECT * from NameToId limit ' + str(groupTalkerId - 1) + ',1'
+    # # 18972836328@chatroom
+    # groupTalkerId = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_FTSContact.db', sql)[0][0]
 
     sql = 'SELECT count(1) FROM MSG WHERE IsSender = 0 AND StrTalker = ?'
-    countMsg = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_MSG0.db', sql, [groupTalkerId])[0][0]
-    startMsg = input("\n共找到" + str(countMsg) + "条聊天记录\n请输入开始导入的聊天记录(从输入的聊天记录到最新的聊天记录全部导出)：\n")
-    sql = '''   SELECT
-                    Type, StrContent, BytesExtra, localId
-                FROM
-                    MSG 
-                WHERE
-                    StrTalker = ? 
-                    AND IsSender = 0
-                    AND localId >= ( SELECT localId FROM MSG WHERE StrContent = ? AND StrTalker = ? )'''
-    exportMsgArray = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_MSG0.db', sql,
-                            (groupTalkerId, startMsg, groupTalkerId))
+    countMsg = 0
+    for i in range(count_msg_db):
+        countMsg += ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_MSG' + str(i) + '.db', sql, [export_group_id])[0][0]
+
+    start_msg_sequence = ''
+    while not start_msg_sequence:
+        startMsg = input("\n共找到" + str(
+            countMsg) + "条聊天记录\n请输入开始导入的聊天记录(从输入的聊天记录到最新的聊天记录全部导出)：\n")
+        # 查询消息开始的下标位置
+        sql = '''
+                    SELECT
+                        MsgSequence, CreateTime
+                    FROM
+                        MSG 
+                    WHERE
+                        StrTalker = ? 
+                        AND StrContent = ?
+         '''
+        match_msg_array = []
+        for i in range(count_msg_db):
+            sql_result = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_MSG' + str(i) + '.db', sql,
+                                (export_group_id, startMsg))
+            match_msg_array.extend(sql_result)
+        if not match_msg_array:
+            print("没有查询到该条聊天记录！\n")
+            continue
+        print("查询到" + str(len(match_msg_array)) + "条聊天记录，请选择：")
+        print('------------------------')
+        i = 1
+        for item in match_msg_array:
+            time_local = time.localtime(item[1])
+            ymd = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
+            print(str(i) + "：" + startMsg + " " + ymd)
+            i += 1
+        print('------------------------')
+        input_index = input()
+        start_msg_sequence = match_msg_array[int(input_index) - 1][0]
+    # 查询要导出的消息
+    exportMsgArray = []
+    for i in range(count_msg_db):
+        sql = '''   SELECT
+                        Type, StrContent, BytesExtra, localId
+                    FROM
+                        MSG 
+                    WHERE
+                        StrTalker = ? 
+                        AND IsSender = 0
+                        AND MsgSequence >= ?'''
+        sql_result = ex_sql('./DECRYPT_WIN_WECHAT_DB/decrypt_copy_MSG' + str(i) + '.db', sql,
+                            (export_group_id, start_msg_sequence))
+        exportMsgArray.extend(sql_result)
     print("\n已找到" + str(len(exportMsgArray)) + "条聊天记录")
     print("正在导出聊天记录...\n")
     # 数据库存储的内容
@@ -130,9 +206,9 @@ if __name__ == '__main__':
             if len(imgArray) > 0:
                 imgPath = imgArray[0].replace('\\\\', '\\')
                 try:
-                    img = decode_dat(wechatFilePath + base64pwd_wxid[1] + '\\' + imgPath)
+                    img = decode_dat(wechatFilePath + wechatId + '\\' + imgPath)
                 except:
-                    content = '图片过期或已被清理'
+                    content = '图片过期或已被清理或未下载原图'
                     msgType = 1
             else:
                 content = '异常：未能成功导出图片'
